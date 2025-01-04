23099b23f4f40c4a480597048b544a2102224cdf
lekimlarra <33814049+lekimlarra@users.noreply.github.com>
2025-01-04 17:30:07 +0100

    grafico basico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,9 +14,22 @@ export class AppComponent {
   title = 'sueldo-neto';
     // Dummy data for the bar chart
     public barChartOptions: ChartOptions = {
+      plugins: {
+        title: {
+          display: true,
+          text: 'Chart.js Bar Chart - Stacked'
+        },
+      },
       responsive: true,
-    };
-    public barChartLabels: string[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+      scales: {
+        x: {
+          stacked: true,
+        },
+        y: {
+          stacked: true
+        }
+      }}
+    public barChartLabels: string[] = ['Seguridad Social empresa', "Seguridad Social trabajador", "IRPF", "Salario neto"];
     public barChartType: ChartType = 'bar';
     public barChartLegend = true;
     public barChartPlugins = [];
@@ -25,15 +38,31 @@ export class AppComponent {
       labels: this.barChartLabels,
       datasets: [{
         label: 'My First Dataset',
-        data: [65, 59, 80, 81, 56, 55, 40],
+        data: [65, 59, 80, 81],
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-          'rgba(255, 205, 86, 0.2)',
           'rgba(75, 192, 192, 0.2)',
           'rgba(54, 162, 235, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(201, 203, 207, 0.2)'
+          'rgba(153, 102, 255, 0.2)'
+        ],
+        borderColor: [
+          'rgb(255, 99, 132)',
+          'rgb(75, 192, 192)',
+          'rgb(54, 162, 235)',
+          'rgb(153, 102, 255)'
+        ],
+        borderWidth: 1
+      }/*,{
+        label: 'My Second Dataset',
+        data: [10, 10, 10, 10, 10, 10, 10],
+        backgroundColor: [
+          'rgba(12, 76, 204, 0.2)',
+          'rgba(12, 76, 204, 0.2)',
+          'rgba(12, 76, 204, 0.2)',
+          'rgba(12, 76, 204, 0.2)',
+          'rgba(12, 76, 204, 0.2)',
+          'rgba(12, 76, 204, 0.2)',
+          'rgba(12, 76, 204, 0.2)'
         ],
         borderColor: [
           'rgb(255, 99, 132)',
@@ -45,6 +74,6 @@ export class AppComponent {
           'rgb(201, 203, 207)'
         ],
         borderWidth: 1
-      }]
+      }*/]
     }
 }
